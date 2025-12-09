107450074e6897c4ade49fe46092095d04d1aabb
Yunyi Han <yunyiverse0302@gmail.com>
2025-12-09 16:21:32 +0900

    실행문

--- DB/기말과제_script/select.sql
+++ DB/기말과제_script/select.sql
@@ -1,17 +1,10 @@
-SELECT * FROM 출처;
-
+SELECT * FROM 개인정보유출원인;
+SELECT * FROM 개인정보유출신고현황;
+SELECT * FROM 개인정보유출출원인;
 SELECT * FROM 민간분야_해킹랜섬웨어_총침해사건수;
-
-SELECT * FROM 개인정보유출원인 ORDER BY 순서;
-
-SELECT * FROM 개인정보유출신고현황 ORDER BY 연도;
-
-SELECT * FROM 해킹세부유형 ORDER BY 순서;
-
 SELECT * FROM 정보보호안전체감도;
-
 SELECT * FROM 정보보호교육방식;
-
-SELECT * FROM 침해사고경험유형;
-
+SELECT * FROM 출처;
 SELECT * FROM 침해사고미신고이유;
+SELECT * FROM 침해사고경험유형;
+SELECT * FROM 해킹세부유형;
